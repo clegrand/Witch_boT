767aa1755450fd79cd71ee580f51ef1d8406cac2
clegrand <comachristo22@gmail.com>
2017-04-20 11:43:23 +0200

    Add Channel class
    Parse ':' at the beginning of Twitch IRC response

--- cooker.py
+++ cooker.py
@@ -3,7 +3,7 @@ import json
 import threading
 from time import sleep
 
-from lepl import Any, Regexp
+from lepl import Any, Regexp, Drop, Literal, Word, make_dict, Optional, FullFirstMatchException
 
 from attic import CONNECTION_PATH
 from cave import TimeLimit
@@ -54,7 +54,7 @@ class TwitchConnect(socket):
         return True
 
     def _recver(self):
-        p = Any()[:, ...] & ~Regexp("\r\n")
+        p = Optional(~Literal(':')) & Any()[:, ...] & ~Regexp("\r\n")
         while True:
             data = self.recv(self.DEF_BUFF)
             if data:
@@ -70,3 +70,46 @@ class TwitchConnect(socket):
     def _ping_pong(self, mess):
         if mess == self.check_in:
             self.send_message(self.check_out)
+
+
+class Channel:
+    BOT_PROMPT = "[BOT] {}"
+    JOIN = "JOIN {}"
+    SEND = "PRIVMSG {channel} :{msg}"
+
+    def __init__(self, channel_name, connection):
+        self.channel_name = "#{}".format(channel_name)
+        self.connect = connection
+        self.connect.send_message(self.JOIN.format(self.channel_name))
+        self.connect.link_func(self._get_message)
+        self.channel_plugins = []
+
+    def link_plugins(self, func):
+        if isinstance(func, (list, tuple)):
+            self.channel_plugins.extend(func)
+        else:
+            self.channel_plugins.append(func)
+
+    def send_message(self, message):
+        self.connect.send_message(self.SEND.format(
+            channel=self.channel_name,
+            msg=self.BOT_PROMPT.format(message)
+        ))
+
+    def _get_message(self, msg):
+        user = Word() > "user"
+        message = Any()[..., ] > "response"
+        get_msg = user & Drop(
+            Literal('!') &
+            user &
+            Literal('@') &
+            user &
+            Regexp(r"\.tmi\.twitch\.tv PRIVMSG ") &
+            self.channel_name &
+            Regexp(r" :")
+        ) & message > make_dict
+        try:
+            msg = get_msg.parse(msg)[0]
+        except FullFirstMatchException:
+            return
+        logger.debug("{user}: {response}".format(**msg))
