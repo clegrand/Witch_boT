e86210a61b7696a033c84ccc2a3e76f2df854183
clegrand <comachristo22@gmail.com>
2017-04-20 11:44:18 +0200

    Add input loop in Console class and use Channel class

--- main.py
+++ main.py
@@ -16,3 +16,4 @@ if __name__ == "__main__":
     else:
         c = Console()
         c.connect()
+        c.input_loop()
--- printer.py
+++ printer.py
@@ -1,11 +1,14 @@
 from cave import User
-from cooker import TwitchConnect
+from cooker import TwitchConnect, Channel
 from welcome import opts, ARGS
 
 
 class Console:
+    KEY_QUIT = 'q'
 
     def __init__(self):
+        self.tc = None
+        self.channel = None
         self.user = User(opts[ARGS].user)
         if not self.user or opts.get(ARGS).create:
             self.user.user = get_string(_("Enter your Twitch user :"))
@@ -15,7 +18,21 @@ class Console:
             self.user.save()
 
     def connect(self):
-        tc = TwitchConnect(self.user.user, self.user.passwd)
+        self.tc = TwitchConnect(self.user.user, self.user.passwd)
+        self.channel = Channel(self.user.channels[0], self.tc)
+
+    def input_loop(self):
+        while True:
+            k = input()
+            if k == self.KEY_QUIT:
+                break
+            if self.tc:
+                self.tc.send_message(k)
+        self.close()
+
+    def close(self):
+        if self.tc:
+            self.tc.close()
 
 
 def get_string(mess, prompt=""):
